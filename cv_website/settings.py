74f47a271013cd56ae78bb3f37689612eba8824b
Mihnea Boca <mihneaboca@yahoo.com>
2020-06-19 22:39:23 +0100

    Static_root

--- cv_website/settings.py
+++ cv_website/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
