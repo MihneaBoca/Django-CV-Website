0500c84a50256f9caf4fbde0e9fd7b15ca45ea36
Mihnea Boca <mihneaboca@yahoo.com>
2020-06-21 23:15:38 +0100

    Unit tests

--- cv_app/tests.py
+++ cv_app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import resolve
+
+from cv_app.views import index
+
 
 # Create your tests here.
+
+class MainPageTest(TestCase):
+
+    def test_index(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,31 +1,45 @@
 import os
 
 from selenium import webdriver
+import unittest
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Adam is a student that wants to write a CV for future use,
-# he's heard there is a website where you can write your CV online
 
-# He goes to the homepage
-# browser = webdriver.Chrome(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'cromedriver.exe'))
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewTest(unittest.TestCase):
 
-assert 'Home' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-# He sees the form he needs to complete in order to end up with a CV
-# He enters his name first
+    def tearDown(self):
+        self.browser.quit()
 
-# He has no work experience yet so he decides to skip that part
+    def test_access_website(self):
+        # Adam is a student that wants to write a CV for future use,
+        # he's heard there is a website where you can write your CV online
 
-# Then he enters information about his education
+        # He goes to the homepage
+        # browser = webdriver.Chrome(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'cromedriver.exe'))
+        #browser = webdriver.Chrome()
+        self.browser.get('http://localhost:8000')
 
-# After that he enters some of his skills
+        self.assertIn('Home', self.browser.title)
 
-# After he reads everything he has just written he decides to submit his CV,
-# but he gets a prompt that says he hasn't entered his email and address and
-# that he must enter them before submitting the CV
+        # He sees the form he needs to complete in order to end up with a CV
+        # He enters his name first
 
-# He enters them and is then able to submit his CV
-# He decides to go an check if his CV was indeed saved on the website
-# He accesses the website again and sees that his CV was saved and can modify it again
+        # He has no work experience yet so he decides to skip that part
+
+        # Then he enters information about his education
+
+        # After that he enters some of his skills
+
+        # After he reads everything he has just written he decides to submit his CV,
+        # but he gets a prompt that says he hasn't entered his email and address and
+        # that he must enter them before submitting the CV
+
+        # He enters them and is then able to submit his CV
+        # He decides to go an check if his CV was indeed saved on the website
+        # He accesses the website again and sees that his CV was saved and can modify it again
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
