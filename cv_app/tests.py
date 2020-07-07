16c62d9753f3169966c9cde29161d45feea74821
Mihnea Boca <mihneaboca@yahoo.com>
2020-07-07 02:09:08 +0100

    Edit CV

--- cv_app/tests.py
+++ cv_app/tests.py
@@ -1,5 +1,7 @@
+from django.db import IntegrityError
 from django.test import TestCase
 from django.urls import resolve
+
 
 from cv_app.views import index
 from cv_app.models import CV
@@ -16,12 +18,24 @@ class MainPageTest(TestCase):
     def test_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
+        response = self.client.get('/bonus/')
+        self.assertTemplateUsed(response, 'bonus.html')
+        response = self.client.get('/display/')
+        self.assertTemplateUsed(response, 'display_list.html')
+        response = self.client.get('/edit/')
+        self.assertTemplateUsed(response, 'edit.html')
 
     def test_can_save_to_database(self):
-        cv1 = CV(first_name='Adam')
-        cv2 = CV(first_name='Jessica')
+        cv1 = CV(username='Adam001')
+        cv2 = CV(username='Jessica001')
         cv1.save()
         cv2.save()
-        self.assertEqual('Adam', CV.objects.get(first_name='Adam').first_name)
+        self.assertEqual('Adam001', CV.objects.get(username='Adam001').username)
         cv1.delete()
         cv2.delete()
+
+    def test_unique_username(self):
+        CV(username='Adam2').save()
+        with self.assertRaises(Exception) as raised:
+            CV(username='Adam2').save()
+        self.assertEqual(IntegrityError, type(raised.exception))
--- cv_app/views.py
+++ cv_app/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
@@ -11,6 +12,8 @@ def index(request):
     if request.method == 'POST':
         # fn = request.POST['first_name']
         # ln = request.POST['last_name']
+        if CV.objects.filter(username=request.POST['username']).exists():
+            return HttpResponse('Username already exits.')
         cv = CV.objects.get_or_create(username=request.POST['username'], first_name=request.POST['first_name'],
                                       last_name=request.POST['last_name'], email=request.POST['email'],
                                       address=request.POST['address'], phone_number=request.POST['phone_number'],
@@ -21,9 +24,46 @@ def index(request):
     return render(request, 'index.html')
 
 
+def edit(request):
+    # DisplayElements.as_view()
+    data = CV.objects.all()
+    if request.method == 'POST':
+        # fn = request.POST['first_name']
+        # ln = request.POST['last_name']
+        CV.objects.filter(username='Adam01').delete()
+        cv = CV.objects.get_or_create(username=request.POST['username'], first_name=request.POST['first_name'],
+                                      last_name=request.POST['last_name'], email=request.POST['email'],
+                                      address=request.POST['address'], phone_number=request.POST['phone_number'],
+                                      pers_desc=request.POST['pers_desc'], experience=request.POST['experience'],
+                                      education=request.POST['education'], skills=request.POST['skills'],
+                                      hobbies=request.POST['hobbies'])[0]
+    return render(request, 'edit.html', {'data': data})
+
+
 class DisplayListView(ListView):
     model = CV
     template_name = 'display_list.html'
+
+    def get_queryset(self):
+        return CV.objects.all()
+
+    def edit(self):
+        # DisplayElements.as_view()
+        if self.method == 'POST':
+            # fn = request.POST['first_name']
+            # ln = request.POST['last_name']
+            cv = CV.objects.get_or_create(username=self.POST['username'], first_name=self.POST['first_name'],
+                                          last_name=self.POST['last_name'], email=self.POST['email'],
+                                          address=self.POST['address'], phone_number=self.POST['phone_number'],
+                                          pers_desc=self.POST['pers_desc'], experience=self.POST['experience'],
+                                          education=self.POST['education'], skills=self.POST['skills'],
+                                          hobbies=self.POST['hobbies'])[0]
+        return render(self, 'edit.html')
+
+
+class DisplayElements(ListView):
+    model = CV
+    template_name = 'edit.html'
 
     def get_queryset(self):
         return CV.objects.all()
